b9d0f3fc6a8370fa9f72a5ec25c21dfa68967fee
𝐓𝐨𝐭𝐨𝐫𝐨 🐥 <sheikhhashim234@gmail.com>
2026-01-02 17:11:12 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,10 @@
 set -e
 
 rm -rf .repo/local_manifests
+rm -rf device/oneplus/aston
+rm -rf vendor/oneplus/aston
+rm -rf kernel/oneplus/sm8550
+rm -rf hardware/oplus
 
 repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault
 
@@ -10,7 +14,5 @@ git clone https://github.com/MeowZiii98/aston_local_manifest --depth 1 -b main .
 /opt/crave/resync.sh
 
 source build/envsetup.sh
-
 lunch infinity_aston-bp3a-userdebug
-
 m bacon
